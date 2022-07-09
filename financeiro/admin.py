966ae01b3dcde6cd870e83fd11212dbc529e048a
natyvit <natalia16.nv@gmail.com>
2022-07-09 17:59:07 -0300

    Criação da model Reserva, Pagamento e Saída

--- financeiro/admin.py
+++ financeiro/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Pagamento
+from .models import Saida
 
 # Register your models here.
+
+@admin.register(Pagamento)
+class PagamentoAdmin(admin.ModelAdmin):
+  list_display= [
+    "id",
+    "valor",
+    "dataCriacao",
+    "reserva",
+  ]
+  
+  def has_change_permission(self, request, obj= None):
+    return False
+
+  def has_delete_permission(self, request, obj= None):
+    return False
+
+  def has_add_permission(self, request):
+    return False
+
+@admin.register(Saida)
+class SaidaAdmin(admin.ModelAdmin):
+  list_display= [
+    "id",
+    "valor",
+    "dataCriacao",
+    "motivo",
+    "observacoes",
+    "usuario_criacao",
+  ]
--- reservas/models.py
+++ reservas/models.py
@@ -55,5 +55,57 @@ class Quarto(models.Model):
     verbose_name= "Quarto"
     verbose_name_plural= "Quartos"
 
-class Hospede(models.Model):
+# class Hospede(models.Model):
+#   """
+#   Esta classe é responsável por todos os dados dos hospedes.
+#   """
+#   nome = models.CharField(
+#     verbose_name= "Nome",
+#   )
+
 class Reserva(models.Model):
+  """
+  Está classe é respondável por todas as funcionalidades de reservas.
+  """
+  dataChegada = models.DateTimeField(
+    verbose_name= "Data Chegada",
+  )
+
+  dataSaida = models.DateTimeField(
+    verbose_name= "Data Saída",
+    null= True,
+    blank= True,
+  )
+
+  valorReserva = models.DecimalField(
+    verbose_name= "Valor da Reserva",
+    max_digits= 7,
+    decimal_places= 2,
+  )
+
+  pagamentoConfirmado = models.BooleanField(
+    verbose_name= "Pagamento Confirmado",
+    default= False,
+  )
+
+  observacoes = models.TextField(
+    verbose_name= "Observações",
+    null= True,
+    blank= True,
+  )
+
+  quarto = models.ForeignKey(
+    Quarto,
+    verbose_name= "Quarto",
+    null= True,
+    blank= True,
+    on_delete= models.SET_NULL,
+  )
+
+  def __str__(self):
+    return f"Reserva {self.id}"
+
+  class Meta:
+    app_label= "reservas"
+    verbose_name= "Reserva"
+    verbose_name_plural= "Reservas"
