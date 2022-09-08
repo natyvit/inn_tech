404fb98b8cef29380e17fc3426c271f42d4bdd94
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-09-08 17:58:53 -0300

    Admindocs e desocupação de quartos

--- reservas/signals/reservas_signals.py
+++ reservas/signals/reservas_signals.py
@@ -12,17 +12,24 @@ def reserva_post_save(sender, instance, created, **kwargs):
     elif kwargs["update_fields"]:
         mudou_pagamentoConfirmado = "pagamentoConfirmado" in kwargs["update_fields"]
         mudou_valorReserva = "valorReserva" in kwargs["update_fields"]
+        mudou_dataSaida = "dataSaida" in kwargs["update_fields"]
 
         if mudou_pagamentoConfirmado:
             instance.gerar_pagamento_se_confirmado()
 
         if mudou_valorReserva:
             instance.atualizar_valor_pagamento()
+        
+        if mudou_dataSaida:
+            instance.desocupar_quarto()
 
 @receiver(pre_save, sender=Reserva)
 def reserva_pre_save(sender, instance, **kwargs):
-    old_object = Reserva.objects.get(id=instance.id)
+    try:
+        old_object = Reserva.objects.get(id=instance.id)
+    except:
+        old_object = None
 
-    if old_object.quarto != instance.quarto:
+    if old_object and (old_object.quarto != instance.quarto):
         old_object.desocupar_quarto()
         instance.ocupar_quarto()
